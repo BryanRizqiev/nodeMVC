2bb0b23cdda9c95d9a282a3e8a41989ec711fdb1
Rizqiev Prakozov <90164438+BryanRizqiev@users.noreply.github.com>
2022-11-03 00:19:58 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize"
 
 const db = new Sequelize('prakozov_db', 'prakozov_bryan', 'yahahawahyu', {
     host: '103.129.222.7',
-    dialect: "mysql"
+    dialect: 'mysql'
 })
 
 export default db
