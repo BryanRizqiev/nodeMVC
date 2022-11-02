f5356496fb0e8a7589819348418a2e9474676cbb
Rizqiev Prakozov <90164438+BryanRizqiev@users.noreply.github.com>
2022-11-03 00:08:56 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
-const db = new Sequelize('schoolldbwithsequelize', 'root', '', {
-    host: 'localhost',
+const db = new Sequelize('prakozov_db', 'prakozov_bryan', 'yahahawahyu', {
+    host: '103.129.222.7',
     dialect: "mysql"
 })
 
