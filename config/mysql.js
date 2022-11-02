d2205c12b742f2ba0eca279387522dd65407fd7e
Rizqiev Prakozov <90164438+BryanRizqiev@users.noreply.github.com>
2022-11-03 00:10:06 +0700

    Update mysql.js

--- config/mysql.js
+++ config/mysql.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2"
 
 const con = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "schoolldbwithsequelize"
+    host: "103.129.222.7",
+    user: "prakozov_bryan",
+    password: "yahahawahyu",
+    database: "prakozov_db"
 })
 
 export default con
